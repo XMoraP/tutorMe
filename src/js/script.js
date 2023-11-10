e8f088f870bee0ed0c4ba1d945bcb6c4bfabc12a
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2023-11-10 13:12:11 +0100

    Cambios en los colores

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,19 @@
-
-
-/* Ventana CHATS */
+/* ----- CHATS ------*/
 var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function(){
-  document.querySelector("body").classList.toggle("active");
+hamburger.addEventListener("click", function () {
+    document.querySelector("body").classList.toggle("active");
 })
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const chatListItems = document.querySelectorAll('.chat-list li');
     const chatMessages = document.getElementById('chat-messages');
     const sendButton = document.getElementById('send-button');
     const inputMessage = document.querySelector('.chat-input input');
 
-    chatListItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            chatListItems.forEach(function(item) {
+    chatListItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            chatListItems.forEach(function (item) {
                 item.classList.remove('active');
             });
 
@@ -26,10 +24,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     sendButton.addEventListener('click', sendMessage);
 
-    inputMessage.addEventListener('keypress', function(event) {
+    inputMessage.addEventListener('keypress', function (event) {
         if (event.key === 'Enter') {
-            event.preventDefault(); // Evita el comportamiento por defecto
-            sendMessage(); // Llama a la función sendMessage
+            event.preventDefault();
+            sendMessage();
         }
     });
 
@@ -47,73 +45,69 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-/* Calendario */
+/* ----- CALENDAR ------*/
 const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span");
+    currentDate = document.querySelector(".current-date"),
+    prevNextIcon = document.querySelectorAll(".icons span");
 
-// getting new date, current year and month
+
 let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
 
-// storing full name of all months in array
 const months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
+    "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
     let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+    for (let i = firstDayofMonth; i > 0; i--) {
         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
 
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                     && currYear === new Date().getFullYear() ? "active" : "";
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+            && currYear === new Date().getFullYear() ? "active" : "";
         liTag += `<li class="${isToday}">${i}</li>`;
     }
 
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+    for (let i = lastDayofMonth; i < 6; i++) {
         liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
     }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
 }
 renderCalendar();
 
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
+        if (currMonth < 0 || currMonth > 11) {
             date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
         } else {
-            date = new Date(); // pass the current date as date value
+            date = new Date();
         }
-        renderCalendar(); // calling renderCalendar function
+        renderCalendar();
     });
 });
 
-/* To Do List*/
+/* ----- TO DO LIST ------*/
 
 let count = 0;
 
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
         alert("Introduce una tarea")
     }
-    else{
-        if (count < 7) {  // Límite de 5 elementos span
+    else {
+        if (count < 7) {
             document.querySelector('#tasks').innerHTML += `
             <ul class="list-group">
                 <li class="list-group-item">
@@ -127,8 +121,8 @@ document.querySelector('#push').onclick = function(){
             </ul>
             `;
             var current_tasks = document.querySelectorAll(".delete");
-            for(var i=0; i<current_tasks.length; i++){
-                current_tasks[i].onclick = function(){
+            for (var i = 0; i < current_tasks.length; i++) {
+                current_tasks[i].onclick = function () {
                     this.parentNode.remove();
                 }
             }
@@ -139,20 +133,9 @@ document.querySelector('#push').onclick = function(){
     }
 }
 
-/*Ventana Perfil*/
+/* ----- PROFILE ------*/
 
-document.querySelectorAll('.guardar').forEach(function(button) {
-    button.addEventListener('click', function() {
-        Swal.fire({
-            title: '¡Actualizado!',
-            text: 'Los cambios se han guardado exitosamente',
-            icon: 'success',
-            confirmButtonText: 'OK'
-        });
-    });
-});
-
-document.getElementById('altaTutor').addEventListener('click', function () {
+function tutor(){
     Swal.fire({
         title: "¿Estás seguro?",
         icon: "warning",
@@ -167,13 +150,13 @@ document.getElementById('altaTutor').addEventListener('click', function () {
                 text: "Tu estatus ha sido modificado",
                 icon: "success"
             }).then(() => {
-                window.open('profileTutor.html', '_blank');
+                window.location.href = 'profileTutor.html';
             });
         }
     });
-});
+}
 
-document.getElementById('bajaTutor').addEventListener('click', function () {
+function noTutor() {
     Swal.fire({
         title: "¿Estás seguro?",
         icon: "warning",
@@ -188,13 +171,20 @@ document.getElementById('bajaTutor').addEventListener('click', function () {
                 text: "Tu estatus ha sido modificado",
                 icon: "success"
             }).then(() => {
-                window.open('profile.html', '_blank');
+                window.location.href = 'profile.html';
             });
         }
     });
-});
+}
 
-
+function showAlert() {
+    Swal.fire({
+      title: '¡Actualizado!',
+      text: 'Los cambios se han guardado exitosamente',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
+  }
 
 
 
