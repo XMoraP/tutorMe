1b84574d1c8202fa819c89a1dba90834616a1b28
Xavier Mora <xavieralexander.moraperaza@usp.ceu.es>
2024-01-10 19:28:16 +0100

    integración con express

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var dashboardRouter = require('./routes/dashboard');
+const registrationRouter = require('./routes/registration');
+
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/registration', registrationRouter);
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
